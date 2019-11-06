f25d978a936ae5745322c445a45b26b08860f165
Mark Halls <mark.halls@gmail.com>
2019-11-06 15:24:51 -0700

    Add Actions

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export const BUY = "BUY";
+export const REMOVE_FEATURE = "REMOVE_FEATURE";
+
+export const buy = () => ({
+    type: BUY
+})
+
+export const remove = () => ({
+    type: REMOVE_FEATURE
+})
